8dc813f763666b8059b5480627c214ee8ee32109
CreachmhaoilStudios <78377986+Cahlien@users.noreply.github.com>
2022-05-30 19:21:20 -0500

    add repositories

---
+++ HotelListing.API/Repositories/HotelsRepository.cs
@@ -0,0 +1,13 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.DataAccessLayer;
+using HotelListing.API.Models;
+
+namespace HotelListing.API.Repositories;
+
+public class HotelsRepository : GenericRepository<Hotel>, IHotelsRepository
+{
+    public HotelsRepository(HotelListingDbContext context) : base(context)
+    {
+        
+    }
+}
