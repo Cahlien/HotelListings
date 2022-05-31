d640603a4bbe9ab34edd69276369793058eb87ea
CreachmhaoilStudios <78377986+Cahlien@users.noreply.github.com>
2022-05-30 19:21:01 -0500

    add repository contracts

---
+++ HotelListing.API/DataAccessLayer/IHotelListingDbContext.cs
@@ -0,0 +1,10 @@
+﻿using HotelListing.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.DataAccessLayer;
+
+public interface IHotelListingDbContext
+{
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Country> Countries { get; set; }
+}
